63a7eef5496f8c52f5618fb88e4cc0a1b626c703
Ianina Kotova <96854171+IaninaK@users.noreply.github.com>
2024-09-13 12:14:41 -0700

    Create scannerClassIntro.java

---
+++ scannerClassIntro.java
@@ -0,0 +1,26 @@
+//Scanner Class Introductiom
+
+import java.util.Scanner;
+
+public class ScannerStarter
+{
+   public static void main(String[] args)
+   {
+        
+      //Write the code here.
+	Scanner kb = new Scanner(System.in);
+	System.out.print("What is your name? ");
+	String name = kb.nextLine();
+
+	System.out.print("How old are you? ");
+	int age = kb.nextInt();
+
+	System.out.print("How much do you get paid hourly? ");
+	double wage = kb.nextDouble();
+
+	System.out.println("Your name is:  " + name);  
+	System.out.println("Age: " + age + " Wage: $" + wage);
+    
+	kb.close();
+   }
+}
